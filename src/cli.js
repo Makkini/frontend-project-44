2e5786fd357f2bd66f9255732760ac14dc6b1622
Makkinni <efstas187@gmail.com>
2024-12-30 15:20:32 +0300

    fix bugs

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,11 @@
 import readlineSync from 'readline-sync';
 
 export const greetings = () => {
+  console.log('Welcome to the Brain Games!');
   if (process.env.NODE_ENV !== 'test') {
     const name = readlineSync.question('May I have your name? ');
     console.log(`Hello, ${name}!`);
     return name;
   }
-  return 'Test User';
+  return 'Test User'; // или какое-то фиксированное имя для тестов
 };
