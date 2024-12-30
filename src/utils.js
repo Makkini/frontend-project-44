9755c3d919488f084e8d37b1b41f0f43ca33b0c1
Makkinni <efstas187@gmail.com>
2024-12-30 15:12:46 +0300

    fix bugs

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,13 @@
 export const getRandomInt = (factor) => Math.floor(Math.random() * factor);
+const getRandomSign = () => {
+  const randomNum = Math.random() * 100;
+  if (randomNum < 33) {
+    return '+';
+  } if (randomNum < 66) {
+    return '-';
+  }
+  return '*';
+};
 export const getRandomGcdNums = () => `${getRandomInt(40)} ${getRandomInt(40)}`;
 export const getRandomExpression = () => `${getRandomInt(40)} ${getRandomSign()} ${getRandomInt(40)}`;
 
@@ -10,14 +19,4 @@ export const generateExpressionProgression = () => {
   }
   arrayExpression[diffProgressionAndIndex] = '..';
   return arrayExpression.join(' ');
-};
-
-const getRandomSign = () => {
-  const randomNum = Math.random() * 100;
-  if (randomNum < 33) {
-    return '+';
-  } if (randomNum < 66) {
-    return '-';
-  }
-  return '*';
 };
