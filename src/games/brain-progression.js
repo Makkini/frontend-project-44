c261d33ce0ff2fe2dd5ea8635098ceb1d8e43392
Makkinni <efstas187@gmail.com>
2024-12-31 11:13:23 +0300

    fix bugs progression game

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -6,8 +6,19 @@ export const progressionGame = () => {
   const getCorrectAnswer = (question) => {
     const arr = question.split(' ').map((el) => (el === '..' ? el : Number(el)));
     const index = arr.indexOf('..');
-    const diff = arr[1] - arr[0];
-    return (arr[index - 1] + diff).toString();
+    const diff = arr.find((el, idx) => idx > 0 && typeof el === 'number') - arr.find((el, idx) => idx === 0 && typeof el === 'number');
+
+    if (index === 0) {
+      return (arr[1] - diff).toString();
+    }
+
+    if (index > 0 && index < arr.length - 1) {
+      return (arr[index - 1] + diff).toString();
+    }
+
+    if (index === arr.length - 1) {
+      return (arr[index - 1] + diff).toString();
+    }
   };
 
   const gameDescription = 'What number is missing in the progression?';
